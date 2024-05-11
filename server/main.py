3f2bcea2a0d739fe8d087ac857e88fba8ab1f4b5
gregaspan <gregor.span@student.um.si>
2024-05-11 23:50:19 +0200

    scrapers

--- server/main.py
+++ server/main.py
@@ -2,10 +2,9 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from openai import OpenAI
 from deep_translator import GoogleTranslator
-from serpapi import GoogleSearch
 import requests
-#import voicerss_tts
-#import assemblyai as aai
+import voicerss_tts
+import assemblyai as aai
 import base64
 from bs4 import BeautifulSoup
 
@@ -18,7 +17,6 @@ VOICERSS_TTS_KEY = "862fba3703124c5d9cfd410aff494ae5"
 ASSEMLBLYAI_STT_KEY = "c6d1e28d398741a7a45554a6fd1d0139"
 X_RAPIDAPI_KEY = "1d9f8aa758msh0f2f642becf4515p1bfb89jsnf70f64195fe4"
 DICTIONARY_API = "GJgVXGJMDDfsC7576edkFw==h7tVsSDr6WCNdoXX"
-IMAGES_API = "2e26df2e32f8642d3ade8783db66d9e8bfa4e0b86283d94ad14011402b4e46e3"
 
 
 @app.route('/api/innovise', methods=['GET'])
@@ -161,7 +159,9 @@ def translate_slo():
 
 @app.route('/api/dictionary', methods=["GET", "POST"])
 def dictionary():
-    translated = GoogleTranslator(source='sl', target='en').translate("avto") 
+    translated = GoogleTranslator(source='sl', target='en').translate("slabo") 
+    print(translated)
+
     
     url = f"https://api.api-ninjas.com/v1/thesaurus?word={translated}"
 
@@ -171,20 +171,84 @@ def dictionary():
 
     neke = requests.get(url, headers=headers).json()["synonyms"]
     tem = []
-    for i in neke:
-        tem.append(GoogleTranslator(source='en', target='sl').translate(i))
-
-    params = {
-        "q": translated,
-        "engine": "google_images",
-        "api_key": IMAGES_API
-    }
-
-    search = GoogleSearch(params)
-    results = search.get_dict()
-    images_results = results["images_results"]
-    return images_results
+    for i in range(5):
+        tem.append(GoogleTranslator(source='en', target='sl').translate(neke[i]))
     return tem
+
+@app.route('/api/scrape', methods=["GET"])
+def scrape24ur():
+    url = ''  
+    domain = url.split('/')[2]  
+
+    page_to_scrape = requests.get(url)
+    soup = BeautifulSoup(page_to_scrape.text, 'html.parser')
+
+    if '24ur.com' in domain:
+        naslov = soup.find_all('h1', attrs={"class": "text-24 lg:text-27 leading-tight font-bold text-black dark:text-white mb-8"})
+        povzetek = soup.find_all('p', attrs={"class": "text-article-summary font-semibold leading-tight text-black dark:text-white"})
+        vsebina_clanka = soup.find_all('div', attrs={"class": "contextual"})
+        tags = soup.find_all('a', attrs={"class": "uppercase text-12 font-bold px-6 py-2 mb-8 mr-4 border border-primary dark:border-primary-400 rounded-sm default-transition text-primary dark:text-primary-400 hover:bg-primary hover:text-white dark:hover:text-white"})
+
+    elif 'rtvslo.si' in domain:
+        naslov = soup.find_all('header', attrs={"class": "article-header"})
+        povzetek = soup.find_all('p', attrs={"class": "lead"})
+        vsebina_clanka = soup.find_all('div', attrs={"class": "article-body"})
+        tags = soup.find_all('a', attrs={"class": "tag"})
+
+    elif 'siol.net' in domain:
+        naslov = soup.find_all('h1', attrs={"class": "article_head__title"})
+        povzetek = soup.find_all('div', attrs={"class": "article_content__lead_text"})
+        vsebina_clanka = soup.find_all('div', attrs={"class": "article_content__inline_elements"})
+        tags = soup.find_all('a', attrs={"class": "article_left_sidebar__keyword"})
+
+    elif 'arnes.si' in domain:
+        naslov = soup.find_all('h1')
+        if len(naslov) > 1:
+            naslov[0] = naslov[1]
+        povzetek = ""
+        vsebina_clanka = soup.find_all('p')
+        tags = ""
+
+    elif 'zurnal24.si' in domain:
+        naslov = soup.find_all('h1', attrs={"class": "article__title"})
+        povzetek = soup.find_all('div', attrs={"class": "article__leadtext"})
+        vsebina_clanka = soup.find_all('div', attrs={"class": "article__content no_page_break cf"})
+        tags = soup.find_all('a', attrs={"class": "article__tag_name"})
+    
+    vsa_vsebina = ""
+    for vsebina in vsebina_clanka:
+        vsa_vsebina = vsa_vsebina + vsebina.text
+
+    vsi_tags = ""
+    for tag in tags:
+        vsi_tags = vsi_tags + tag.text
+
+    return jsonify({
+        "naslov": naslov[0].text if naslov else "",
+        "povzetek": povzetek[0].text if povzetek else "",
+        "vsebina_clanka": vsa_vsebina,
+        "tags": vsi_tags
+    })
+
+
+@app.route('/api/sskj', methods=["GET"])
+def sskj():
+    beseda = "miza"
+    url = "https://www.fran.si/iskanje?FilteredDictionaryIds=130&View=1&Query=" + beseda
+    page_to_scrape = requests.get(url)
+    soup = BeautifulSoup(page_to_scrape.text, 'html.parser')
+
+    beseda = beseda.capitalize()
+    zaglavje = soup.find_all('span', attrs={"data-group": "header"})
+    definicija = soup.find_all('span', attrs={"data-group": "explanation "})
+    celota = soup.find_all('div', attrs={"class": "entry-content"})
+
+    return jsonify({
+        "beseda": beseda,
+        "zaglavje": zaglavje[0].text if zaglavje else "",
+        "definicija": definicija[0].text if definicija else "",
+        "celota": celota[0].text if celota else ""
+    })
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
